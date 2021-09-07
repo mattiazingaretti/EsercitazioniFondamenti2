fcc70cf4e6cb95232d6a6fa263f5551e9a032ed8
zingaretti.mattia <studente@localhost.localdomain>
2021-09-07 15:04:40 +0200

    Started Lab0

--- Esercitazione00_2021/java-source/BubbleSort.java
+++ Esercitazione00_2021/java-source/BubbleSort.java
@@ -25,6 +25,15 @@ class BubbleSort {
   
     // Driver per provare la classe 
     public static void main(String args[]) { 
+		
+		int i;
+		int l  = args.length;
+		
+		for(i = 0; i < l ; i++){
+			
+		}
+		 
+		
         BubbleSort ob = new BubbleSort(); 
         int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
         ob.bubbleSort(arr); 
