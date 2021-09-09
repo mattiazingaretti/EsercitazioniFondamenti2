24873747218d56c1e6e13574cba08242bfc24ad6
zingaretti.mattia <studente@localhost.localdomain>
2021-09-09 20:59:01 +0200

    Started lab01, currently at task 2 testing sum of matrices functionality

--- Esercitazione01_2021/java-aux-050318/Main.java
+++ Esercitazione01_2021/java-aux-050318/Main.java
@@ -14,30 +14,70 @@ public class Main {
 		System.out.println("##################################");
 		System.out.println("");
 		System.out.println("");
-		System.out.println("Added -> 0,0,5");
+		//System.out.println("Added -> 0,0,5");
 		mat1.set(0, 0, 5);
-		System.out.println(mat1);
+		//System.out.println(mat1);
 		System.out.println("");
-		System.out.println("Added -> 10,10,45");
+		//System.out.println("Added -> 10,10,45");
 		mat1.set(10, 10, 45);
-		System.out.println(mat1);
+		//System.out.println(mat1);
 		System.out.println("");
-		System.out.println("Added -> 10,10,7");
+		//System.out.println("Added -> 10,10,7");
 		mat1.set(10, 10, 7);
-		System.out.println(mat1);
+		//System.out.println(mat1);
 		System.out.println("");
-		System.out.println("Added -> 24,29,1");
+		//System.out.println("Added -> 24,29,1");
 		mat1.set(24, 29, 1);
-		System.out.println(mat1);
+		//System.out.println(mat1);
 		System.out.println("");
-		System.out.println("Added -> 24,29,5");
+		//System.out.println("Added -> 24,29,5");
 		mat1.set(24, 29, 5);
-		System.out.println(mat1);
+		//System.out.println(mat1);
 		System.out.println("");
-		System.out.println("Added -> 18,17,12");
+		//System.out.println("Added -> 18,17,12");
 		mat1.set(18, 17, 12);
 		System.out.println(mat1);
 		System.out.println("");
+		
+		
+		MatriceSparsa mat2 = readMatFF();
+		System.out.println("Original Matrix:");
+		System.out.println(mat1);
+		System.out.println("##################################");
+		System.out.println("##################################");
+		System.out.println("");
+		System.out.println("");
+		//System.out.println("Added -> 0,0,5");
+		mat1.set(0, 0, 5);
+		//System.out.println(mat1);
+		System.out.println("");
+		//System.out.println("Added -> 10,10,45");
+		mat1.set(10, 10, 45);
+		//System.out.println(mat1);
+		System.out.println("");
+		//System.out.println("Added -> 10,10,7");
+		mat1.set(10, 10, 7);
+		//System.out.println(mat1);
+		System.out.println("");
+		//System.out.println("Added -> 24,29,1");
+		mat1.set(24, 29, 1);
+		//System.out.println(mat1);
+		System.out.println("");
+		//System.out.println("Added -> 24,29,5");
+		mat1.set(24, 29, 5);
+		//System.out.println(mat1);
+		System.out.println("");
+		//System.out.println("Added -> 18,17,12");
+		mat1.set(18, 17, 12);
+		System.out.println(mat1);
+		System.out.println("");
+		
+		System.out.println("Risultato somma");
+		MatriceSparsa r = new MatriceSparsa(mat1.getNumRow() , mat1.getNumCol());
+		System.out.println(r.add(mat1, mat2));
+		
+		
+		
 	}
 
 	private static MatriceSparsa readMatFF() {
--- Esercitazione01_2021/java-aux-050318/MatriceSparsa.java
+++ Esercitazione01_2021/java-aux-050318/MatriceSparsa.java
@@ -1,43 +1,150 @@
+import java.lang.Exception;
 
 public class MatriceSparsa {
 
 	private class Elem{
-
+		int i;
+		int j;
+		int x;
+		Elem next;
+		
+		private Elem(int i , int j , int x , Elem next){
+			this.i = i;
+			this.j = j;
+			this.x = x;
+			this.next = next;
+		}
+		
 	}
-
+	
+	private int m;
+	private int n;
+	Elem head;
 
 	public MatriceSparsa(int m, int n) {
-
+		this.m = m;
+		this.n = n;
 	}
 
 	public int getNumRow() {
-		// TODO: Implement here
-		return 0;
+		return m;
 	}
 
 	public int getNumCol() {
-		// TODO: Implement here
-		return 0;
+		return n;
 	}
 
 	public void set(int i, int j, int x) {
-		// TODO: Implement here
+		//extreme cases handling
+		//if(i < 0 || j < 0) throw new Exception("error in set values i or j is negative ! ");
+		
+		if(head == null){
+			this.head = new Elem(i , j, x, null);
+		}else{
+			Elem it = head;
+			int rowMajorIt; //Just to simplify if conditions.
+			int rowMajorElem = this.n*i + j;
+			
+			while(it != null){
+				rowMajorIt = this.n*it.i + it.j;
+				if( rowMajorElem > rowMajorIt){
+					if(it.next != null){
+						if( (this.n*it.next.i+it.next.j) > rowMajorElem) {
+							//iNSERT ELEM THERE
+							Elem newItem = new Elem(i, j ,x ,it.next);
+							it.next = newItem; //Leave other link to garbage collector.
+						}
+					}else{
+						Elem newItem = new Elem(i, j ,x ,null);
+						it.next = newItem; //Leave other link to garbage collector.
+						
+					}
+				}else if(rowMajorIt == rowMajorElem){
+					it.i = i;
+					it.j = j;
+					it.x = x;
+				}
+				it = it.next;
+			}
+		}
 	}
 
 	public int get(int i, int j) {
-		// TODO: Implement here
-		return 0;
+		if(this.head == null) return 0; //Assuming this is the default value on the matrix...  
+		
+		Elem it = head;
+		int found = 0; 
+		
+		while(it != null){
+			if (it.i == i && it.j == j) {
+				found = it.x;
+				break;
+			}
+			it = it.next;
+		}
+		
+		if(found != 0)
+			return found;
+		else
+			return 0;
+		
 	}
+
 
 	public String toString() {
-		// TODO: Implement here
-		return null;
+		Elem it = head;
+		String s = new String();
+		while(it != null){
+			s+= "x: "+it.x+" i: "+it.i + " j: "+it.j + " -> \n" ;
+			it = it.next;
+		}
+		s += " null";
+		return s;
 	}
 
+
 	public MatriceSparsa add(MatriceSparsa mat1, MatriceSparsa mat2) {
-		// TODO: Implement here
-		return null;
+		if(mat1.m != mat2.m || mat1.n != mat2.n ) return null;
+		
+		Elem it1 = mat1.head;
+		Elem it2 = mat2.head;
+		int rM1 , rM2 ;
+		
+		
+		MatriceSparsa r = new MatriceSparsa(mat1.m , mat2.n);
+		
+		
+		while(it1 != null && it2 != null){
+			rM1 = mat1.n*it1.i + it1.j;
+			rM2 = mat2.n*it2.i + it1.j;
+			
+			if(rM1 == rM2)
+				r.set(it1.i , it2.j , it1.x+it2.x );
+			else {
+				r.set(it1.i , it1.j , it1.x );
+				r.set(it2.i , it2.j , it2.x );
+			}
+			it1 = it1.next;
+			it2 = it2.next;
+		}
+		
+		if(it1 == null && it2 != null){
+			while(it2 != null){
+				r.set(it2.i , it2.j , it2.x );
+				it2 = it2.next;
+			}
+		}else if(it1 != null && it2 == null){
+			while(it1 != null){
+				r.set(it1.i , it1.j , it1.x );
+				it1 = it1.next;
+			}
+		}
+		
+		
+		return r;
 	}
+
+
 
 	public MatriceSparsa tra(MatriceSparsa mat1, MatriceSparsa mat2) {
 		// TODO: Implement here
