10f43976fefc18b4001bb155cb24d4eebdd26cc6
Mattia <zingaretti.mattia@gmail.com>
2021-09-23 14:36:21 +0200

    Done Lab01

--- Esercitazione01_2021/java-aux-050318/MatriceSparsa.java
+++ Esercitazione01_2021/java-aux-050318/MatriceSparsa.java
@@ -37,6 +37,8 @@ public class MatriceSparsa {
 	public void set(int i, int j, int x) {
 		//extreme cases handling
 		//if(i < 0 || j < 0) throw new Exception("error in set values i or j is negative ! ");
+		if (i >= this.m ) return;
+		if(j >= this.n) return;
 		if(x == 0) return ;
 		if(head == null){
 			this.head = new Elem(i , j, x, null);
@@ -147,20 +149,45 @@ public class MatriceSparsa {
 
 
 	public MatriceSparsa tra(MatriceSparsa mat1) {
+		MatriceSparsa ret  = new MatriceSparsa(mat1.n , mat1.m);
 		Elem it = mat1.head;
 		
 		while(it != null){
 			if(it.i != it.j){
-				mat1.set(it.j, it.i , it.x);
+				ret.set(it.j, it.i , it.x);
+			}else{
+				ret.set(it.i, it.j , it.x);
 			}
 			it = it.next;
 		}
 		
-		return mat1;
+		return ret;
 	}
 
 	public MatriceSparsa mul(MatriceSparsa mat1, MatriceSparsa mat2) {
-		// TODO: Implement here
-		return null;
+		int row1, row2, col1, col2;
+		row1 = mat1.getNumRow();
+		row2 = mat2.getNumRow();
+		col1 = mat1.getNumCol();
+		col2 = mat2.getNumCol();
+		if(col1 != row2) {
+			throw new RuntimeException("Impossibile sommare le due matrici, dimensioni differenti");
+		}
+		MatriceSparsa ret = new MatriceSparsa(row1, col2);
+		int i,j,k,val;
+		for (i = 0; i < row1; i++) {
+			for(j = 0; j < col2; j++) {
+				val = 0;
+				for (k = 0; k < col1; k++) {
+					val += (mat1.get(i, k) * mat2.get(k, j));
+				}
+				if (val != 0) {
+					ret.set(i, j, val);
+				}
+			}
+		}
+		return ret;
+
+		return res;
 	}
 }
