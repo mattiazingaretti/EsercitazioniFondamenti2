3608724dd7106167dcb005a5a2e04c8546fae6f2
Mattia <zingaretti.mattia@gmail.com>
2021-09-25 10:41:32 +0200

    Done Lab02 Fondamenti2.

--- Esercitazione02_2021/java-aux-170320/Labirinto.java
+++ Esercitazione02_2021/java-aux-170320/Labirinto.java
@@ -7,35 +7,80 @@ public class Labirinto {
     private boolean marcata[][];
 
   public Labirinto(int n) {
-		//TODO: Da Completare
+	 this.m = new Cella[n][n];
+	 this.marcata = new boolean[n][n];
+	 this.n = n;
   }
 
 	public void setPiena(int r, int c){
-		//TODO: Da Completare
+		this.m[r][c] = Cella.PIENA;
   }
 
 	private boolean uscita(int r, int c){
-  		//TODO: Da Completare
-		return false;
+  		if(this.m[r][c] == Cella.VUOTA && r == this.n-1 && c == this.n-1 )
+			return true;
+		else return false;
   }
 
 	public boolean percorribile(int r, int c){
-  		//TODO: Da Completare
-		return false;
+  		if(r < 0 || r > this.n -1) //dentro i bordi
+		  return false;
+		if(c < 0 || c > this.n-1)
+		  return false;
+		if(marcata[r][c] == true || m[r][c] == Cella.PIENA) //NE piena ne marcata
+		  return false; 
+		return true;
   }
+  
 
+    //Deve essere ricorsiva questa funzionalità True se e solo se da m[r][c] si raggiunge l'uscita.
 	private boolean uscitaRaggiungibileDa(int r, int c){
-		//TODO: Da Completare
-		return false;
+		if(uscita(r,c)){	
+			return true;
+		}else{
+			if(percorribile(r, c+1)){
+				marcata[r][c+1] = true;
+				uscitaRaggiungibileDa(r, c+1);
+			}
+	
+			if(percorribile(r, c-1)){
+				marcata[r][c-1] = true;
+				uscitaRaggiungibileDa(r , c-1);
+			}
+			
+			if(percorribile(r+1 ,c)){
+				marcata[r+1][c] = true;
+				uscitaRaggiungibileDa(r+1, c);	
+			}
+
+			if(percorribile(r-1 , c)){
+				marcata[r-1][c] = true;
+				uscitaRaggiungibileDa(r-1, c);
+			}
+		}
+
+		if(marcata[n-1][n-1] == true)
+			return true;
+		else
+			return false;
 	}
 
 	public boolean risolvibile(){
-		//TODO: Da Completare
-		return false;
+		if(!uscitaRaggiungibileDa(0,0))
+		  return false;
+		return true;
 	}
 
 	public String toString() {
-		//TODO: Da Completare
-		return null;
+		String s = new String();
+		for(int i = 0; i < n ; i++){
+			for(int j = 0; j< n ; j++)
+				if(marcata[i][j])
+					s += "1 ";
+				else
+					s += "0 ";
+			s+= "\n";
+		}
+		  return s;
 	}
 }
